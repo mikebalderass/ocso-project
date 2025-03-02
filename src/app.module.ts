776e9281e7e82714237ef4376fdc7559c7b066c3
mbalderasuaq <mbalderas08@alumnos.uaq.mx>
2025-03-01 18:08:53 -0600

    feat: add TypeORM implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EmployeesModule } from './employees/employees.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { EmployeesModule } from "./employees/employees.module";
+import { ProductsModule } from "./products/products.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.host,
+      port: process.env.port ? +process.env.port : 3000,
+      username: "postgres",
+      password: process.env.pass,
+      database: process.env.name,
+      entities: [],
+      synchronize: true,
+    }),
+    EmployeesModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
