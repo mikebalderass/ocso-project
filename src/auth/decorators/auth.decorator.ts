479a8cbd976a58b191e553e0d17094bdf4ca65fe
mbalderasuaq <mbalderas08@alumnos.uaq.mx>
2025-03-08 18:34:34 -0600

    feat: add auth decorator

---
+++ src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,9 @@
+import { applyDecorators, UseGuards } from "@nestjs/common";
+import { Roles } from "./roles.decorator";
+import { AuthGuard } from "../guards/auth.guard";
+import { RolesGuard } from "../guards/roles.guard";
+
+export const Auth = (...roles: string[]) => {
+  roles.push("Admin");
+  return applyDecorators(Roles(roles), UseGuards(AuthGuard, RolesGuard));
+};
