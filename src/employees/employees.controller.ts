e826d56cc0795065728064a697c0e2972f3d30a3
mbalderasuaq <mbalderas08@alumnos.uaq.mx>
2025-03-08 21:08:17 -0600

    feat: add swagger to all employee routes

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -21,7 +21,7 @@ import { Employee } from "./entities/employee.entity";
 import { ApiAuth } from "src/auth/decorators/api.decorator";
 
 @ApiAuth()
-@ApiTags('Employees')
+@ApiTags("Employees")
 @Controller("employees")
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -35,7 +35,7 @@ export class EmployeesController {
       employeeEmail: "mike@gmail.com",
       employeeLastName: "Balderas",
       employeePhoneNumber: "4871443289",
-    } as Employee
+    } as Employee,
   })
   @Post()
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
@@ -43,19 +43,49 @@ export class EmployeesController {
   }
 
   @Auth(ROLES.MANAGER, ROLES.EMPLOYEE)
+  @ApiResponse({
+    status: 200,
+    example: "OK",
+  })
   @Post("upload")
+  @ApiResponse({
+    status: 200,
+    example: "OK",
+  })
   @UseInterceptors(FileInterceptor("file"))
   uploadPhoto(@UploadedFile() file: Express.Multer.File) {
     return "OK";
   }
 
   @Auth(ROLES.MANAGER)
+  @ApiResponse({
+    status: 200,
+    example: [
+      {
+        employeeId: "UUID",
+        employeeName: "Mike",
+        employeeEmail: "",
+        employeeLastName: "Balderas",
+        employeePhoneNumber: "4871443289",
+      } as Employee,
+    ],
+  })
   @Get()
   findAll() {
     return this.employeesService.findAll();
   }
 
   @Auth(ROLES.MANAGER)
+  @ApiResponse({
+    status: 200,
+    example: {
+      employeeId: "UUID",
+      employeeName: "Mike",
+      employeeEmail: "",
+      employeeLastName: "Balderas",
+      employeePhoneNumber: "4871443289",
+    } as Employee,
+  })
   @Get(":id")
   findOne(
     @Param("id", new ParseUUIDPipe({ version: "4" }))
@@ -65,12 +95,34 @@ export class EmployeesController {
   }
 
   @Auth(ROLES.MANAGER)
+  @ApiResponse({
+    status: 200,
+    example: [
+      {
+        employeeId: "UUID",
+        employeeName: "Mike",
+        employeeEmail: "",
+        employeeLastName: "Balderas",
+        employeePhoneNumber: "4871443289",
+      } as Employee,
+    ],
+  })
   @Get("/location/:id")
   findAllLocation(@Param("id") id: string) {
     return this.employeesService.findByLocation(+id);
   }
 
   @Auth(ROLES.EMPLOYEE)
+  @ApiResponse({
+    status: 200,
+    example: {
+      employeeId: "UUID",
+      employeeName: "Mike",
+      employeeEmail: "",
+      employeeLastName: "Balderas",
+      employeePhoneNumber: "4871443289",
+    } as Employee,
+  })
   @Patch(":id")
   update(
     @Param("id", new ParseUUIDPipe({ version: "4" })) id: string,
@@ -80,6 +132,10 @@ export class EmployeesController {
   }
 
   @Auth(ROLES.MANAGER)
+  @ApiResponse({
+    status: 200,
+    example: "OK",
+  })
   @Delete(":id")
   remove(@Param("id", new ParseUUIDPipe({ version: "4" })) id: string) {
     return this.employeesService.remove(id);
